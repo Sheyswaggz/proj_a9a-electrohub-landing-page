f60213725eb9f86e374eadbb366eb736ac76b9d4
Sheyswaggz <oluwaseun65771@gmail.com>
2026-01-03 13:40:49 +0100

    Add .eslintrc.js for task: Setup Project Foundation and Build Configuration

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.config.js', 'vite.config.js', 'tailwind.config.js', 'postcss.config.js'],
+      env: {
+        node: true,
+      },
+    },
+  ],
+};
